62ad92e0436acd5a1aa9082692a3b8caad3c65e1
pablo2808o <pavelchasovnikov2808@gmail.com>
2026-05-19 23:00:03 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,17 @@
+from aiogram import Router
+from aiogram.types import Message
 
+from config import ADMIN_ID
+from keyboards.main import main_keyboard
+
+router = Router()
+
+@router.message()
+async def start(message: Message):
+
+    is_admin = message.from_user.id == ADMIN_ID
+
+    await message.answer(
+        "Добро пожаловать в EDGE esports!",
+        reply_markup=main_keyboard(is_admin)
+    )
