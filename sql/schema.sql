68fb18480c1d420b138152bfb23d9cff1ac41a71
pablo2808o <pavelchasovnikov2808@gmail.com>
2026-05-19 23:02:57 +0300

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    telegram_id BIGINT UNIQUE,
+    username TEXT,
+    uid TEXT,
+    league TEXT DEFAULT 'EDGE base',
+    points INTEGER DEFAULT 0
+);
