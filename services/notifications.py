6099007e456ad5e346f52fe2ff228285b5134c92
pablo2808o <pavelchasovnikov2808@gmail.com>
2026-05-19 23:18:24 +0300

    Update notifications.py

--- services/notifications.py
+++ services/notifications.py
@@ -1,1 +1,15 @@
+async def send_lobby_data(
+    bot,
+    user_id,
+    room_id,
+    password
+):
 
+    await bot.send_message(
+        chat_id=user_id,
+        text=(
+            "🏆 Данные лобби\n\n"
+            f"🆔 ID: {room_id}\n"
+            f"🔑 Пароль: {password}"
+        )
+    )
