49055691ea5db3a73c0b1f0079d7527b3920d734
pablo2808o <pavelchasovnikov2808@gmail.com>
2026-05-19 22:58:35 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,14 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.orm import declarative_base
 
+DATABASE_URL = "sqlite+aiosqlite:///edge.db"
+
+engine = create_async_engine(DATABASE_URL)
+
+async_session = async_sessionmaker(
+    engine,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
