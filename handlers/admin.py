a786314e4c398aec898c247d3d39058fe20107ad
pablo2808o <pavelchasovnikov2808@gmail.com>
2026-05-19 23:01:14 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,1 +1,13 @@
+from aiogram import Router
+from aiogram.types import Message
 
+router = Router()
+
+@router.message()
+async def admin_panel(message: Message):
+
+    if message.text == "🔧 Админ панель":
+
+        await message.answer(
+            "🔧 Админ панель EDGE esports"
+        )
