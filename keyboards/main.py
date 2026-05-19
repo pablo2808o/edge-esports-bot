678593c16d468c032c9dcf270b0c0397394ef388
pablo2808o <pavelchasovnikov2808@gmail.com>
2026-05-19 22:59:34 +0300

    Update main.py

--- keyboards/main.py
+++ keyboards/main.py
@@ -1,1 +1,41 @@
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton
 
+def main_keyboard(is_admin=False):
+
+    keyboard = [
+
+        [KeyboardButton(
+            text="📛 Вступить в клан"
+        )],
+
+        [KeyboardButton(
+            text="⚔️ TDM"
+        )],
+
+        [KeyboardButton(
+            text="🏆 Турнир EDGE esports"
+        )],
+
+        [KeyboardButton(
+            text="🎮 Турнир Big Play"
+        )],
+
+        [KeyboardButton(
+            text="📚 Клановые учения"
+        )],
+    ]
+
+    if is_admin:
+
+        keyboard.append([
+
+            KeyboardButton(
+                text="🔧 Админ панель"
+            )
+        ])
+
+    return ReplyKeyboardMarkup(
+        keyboard=keyboard,
+        resize_keyboard=True
+    )
