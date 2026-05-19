31388ad56820ea7cd12da26e958cf6fdbccfc2f3
pablo2808o <pavelchasovnikov2808@gmail.com>
2026-05-19 23:17:13 +0300

    Update edge_teams.py

--- services/edge_teams.py
+++ services/edge_teams.py
@@ -1,1 +1,21 @@
+async def get_free_team_number(
+    session
+):
 
+    used_numbers = []
+
+    result = await session.execute(
+        "SELECT team_number FROM edge_teams"
+    )
+
+    rows = result.fetchall()
+
+    for row in rows:
+
+        used_numbers.append(row[0])
+
+    for i in range(2, 51):
+
+        if i not in used_numbers:
+
+            return i
