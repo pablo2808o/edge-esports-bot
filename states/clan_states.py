c1833702b02795f9e9ac8f475d227233e501ffb3
pablo2808o <pavelchasovnikov2808@gmail.com>
2026-05-19 23:00:23 +0300

    Update clan_states.py

--- states/clan_states.py
+++ states/clan_states.py
@@ -1,1 +1,12 @@
+from aiogram.fsm.state import StatesGroup
+from aiogram.fsm.state import State
 
+class ClanRegistration(StatesGroup):
+
+    waiting_uid = State()
+
+    waiting_experience_type = State()
+
+    waiting_experience_text = State()
+
+    waiting_screenshot = State()
