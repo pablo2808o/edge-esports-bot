3897229baae5fa67e3a77bb492a8d0093ce063e4
pablo2808o <pavelchasovnikov2808@gmail.com>
2026-05-19 22:59:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+import asyncio
 
+from aiogram import Bot
+from aiogram import Dispatcher
+
+from config import BOT_TOKEN
+
+from handlers.start import router as start_router
+from handlers.clan import router as clan_router
+from handlers.admin import router as admin_router
+from handlers.bigplay import router as bigplay_router
+
+async def main():
+
+    bot = Bot(token=BOT_TOKEN)
+
+    dp = Dispatcher()
+
+    dp.include_router(start_router)
+    dp.include_router(clan_router)
+    dp.include_router(admin_router)
+    dp.include_router(bigplay_router)
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
