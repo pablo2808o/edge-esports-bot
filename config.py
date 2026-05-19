6e0b868af4b4152321bb7f9569e6a71a3d49cd57
pablo2808o <pavelchasovnikov2808@gmail.com>
2026-05-19 22:57:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+LEAGUES = [
+    "EDGE base",
+    "EDGE core",
+    "EDGE flow",
+    "EDGE pro",
+    "EDGE prime",
+    "EDGE elit",
+    "EDGE MIET",
+]
