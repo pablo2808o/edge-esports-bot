308378bfe8e926ca059db07a72d668537af02952
pablo2808o <pavelchasovnikov2808@gmail.com>
2026-05-19 23:15:30 +0300

    Update leagues.py

--- services/leagues.py
+++ services/leagues.py
@@ -1,1 +1,12 @@
+from config import LEAGUES
 
+
+def get_available_tdm_ranks(
+    player_rank
+):
+
+    current_index = LEAGUES.index(
+        player_rank
+    )
+
+    return LEAGUES[current_index:]
