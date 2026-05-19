ca1c5ddd9354f909fcc1fcad913898d11a995d66
pablo2808o <pavelchasovnikov2808@gmail.com>
2026-05-19 23:23:00 +0300

    Update edge.py

--- models/edge.py
+++ models/edge.py
@@ -1,1 +1,28 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import Text
 
+from database import Base
+
+
+class EdgeTeam(Base):
+
+    __tablename__ = "edge_teams"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    tournament_id = Column(Integer)
+
+    team_number = Column(Integer)
+
+    player1_id = Column(Integer)
+
+    player2_id = Column(Integer)
+
+    status = Column(
+        Text,
+        default="active"
+    )
